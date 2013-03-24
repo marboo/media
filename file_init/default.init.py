a9de42872aa72aa11f7e72ef5f3c916154e2165a
amoblin <amoblin@gmail.com>
2013-03-24 18:14:07 +0800

    use local server
    perfect for marboo v0.6.1 and marboo webapp v0.2

--- file_init/default.init.py
+++ file_init/default.init.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 # author: amoblin <amoblin@gmail.com>
-# file name: %@.py
-# create date: %@
+# file name: %s.py
+# create date: %s
 # This file is created from ~/.marboo/source/media/file_init/default.init.py
 # 本文件由 ~/.marboo/source/media/file_init/default.init.py 复制而来
--- file_init/default.init.rst
+++ file_init/default.init.rst
@@ -1,10 +1,10 @@
-%@
-%@
-%@
+%s
+%s
+%s
 
 .. Author: your_name 
 .. title:: this is the real title in Jekyll.
-.. |date| date:: %@
+.. |date| date:: %s
 .. publish:: NO
 ..  This file is created from ~/.marboo/source/media/file_init/default.init.rst
 .. 本文件由 ~/.marboo/source/media/file_init/default.init.rst 复制而来
