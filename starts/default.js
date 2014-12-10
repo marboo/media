1907b70b73437ed96d957db6c336bd017b214881
amoblin@mbp <amoblin@gmail.com>
2014-12-10 13:11:00 +0800

    Marboo v0.9, use system_config.json instead of system_rules.config

--- starts/default.js
+++ starts/default.js
@@ -1,10 +1,10 @@
-#!/usr/bin/env nodemon
-/*****************************************************************************
+/* #!/usr/bin/env nodemon
+/****************************************************************************
 # -*- coding:utf-8 -*-
 # author: amoblin <amoblin@gmail.com>
-# file name: {{title}}.js
+# filename: {{title}}.js
 # description: TODO
 # create date: {{date}}
 # This file is created from $MARBOO_HOME/media/starts/default.js
 # 本文件由 $MARBOO_HOME/media/starts/default.js 复制而来
-*****************************************************************************/
+****************************************************************************/
